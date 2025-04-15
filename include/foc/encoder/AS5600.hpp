39a93eab6345db946c0e0bbf66de9075e2b4e7b9
jadjer <jadjer@ya.ru>
2025-04-15 23:55:15 +0300

    .clang-tidy updated

--- include/foc/encoder/AS5600.hpp
+++ include/foc/encoder/AS5600.hpp
@@ -14,140 +14,139 @@
 
 #pragma once
 
+#include <foc/encoder/EncoderBase.hpp>
 #include <i2c/Device.hpp>
 #include <i2c/Master.hpp>
-#include <foc/encoder/EncoderBase.hpp>
 
 namespace foc {
 
-    class AS5600 : public foc::EncoderBase {
-    public:
-        using Byte = std::uint8_t;
-        using Device = std::unique_ptr<i2c::Device>;
-        using BusMaster = std::unique_ptr<i2c::Master>;
+class AS5600 : public EncoderBase {
+public:
+  using Byte = std::uint8_t;
+  using Device = std::unique_ptr<i2c::Device>;
+  using BusMaster = std::unique_ptr<i2c::Master>;
 
-    public:
-        enum PowerMode : std::uint8_t {
-            POWER_MODE_NORMAL [[maybe_unused]] = 0b00,
-            POWER_MODE_LOW_1 [[maybe_unused]] = 0b01,
-            POWER_MODE_LOW_2 [[maybe_unused]] = 0b10,
-            POWER_MODE_LOW_3 [[maybe_unused]] = 0b11,
-        };
+public:
+  enum PowerMode : std::uint8_t {
+    POWER_MODE_NORMAL [[maybe_unused]] = 0b00,
+    POWER_MODE_LOW_1 [[maybe_unused]] = 0b01,
+    POWER_MODE_LOW_2 [[maybe_unused]] = 0b10,
+    POWER_MODE_LOW_3 [[maybe_unused]] = 0b11,
+  };
 
-        enum Hysteresis : std::uint8_t {
-            HYSTERESIS_OFF [[maybe_unused]] = 0b00,
-            HYSTERESIS_LSB [[maybe_unused]] = 0b01,
-            HYSTERESIS_2_LSB [[maybe_unused]] = 0b10,
-            HYSTERESIS_3_LSB [[maybe_unused]] = 0b11,
-        };
+  enum Hysteresis : std::uint8_t {
+    HYSTERESIS_OFF [[maybe_unused]] = 0b00,
+    HYSTERESIS_LSB [[maybe_unused]] = 0b01,
+    HYSTERESIS_2_LSB [[maybe_unused]] = 0b10,
+    HYSTERESIS_3_LSB [[maybe_unused]] = 0b11,
+  };
 
-        enum OutputStage : std::uint8_t {
-            OUTPUT_STAGE_ANALOG_FULL [[maybe_unused]] = 0b00,
-            OUTPUT_STAGE_ANALOG_REDUCED [[maybe_unused]] = 0b01,
-            OUTPUT_STAGE_DIGITAL_PWM [[maybe_unused]] = 0b10,
-        };
+  enum OutputStage : std::uint8_t {
+    OUTPUT_STAGE_ANALOG_FULL [[maybe_unused]] = 0b00,
+    OUTPUT_STAGE_ANALOG_REDUCED [[maybe_unused]] = 0b01,
+    OUTPUT_STAGE_DIGITAL_PWM [[maybe_unused]] = 0b10,
+  };
 
-        enum PWMFrequency : std::uint8_t {
-            FREQUENCY_115_HZ [[maybe_unused]] = 0b00,
-            FREQUENCY_230_HZ [[maybe_unused]] = 0b01,
-            FREQUENCY_460_HZ [[maybe_unused]] = 0b10,
-            FREQUENCY_920_HZ [[maybe_unused]] = 0b11,
-        };
+  enum PWMFrequency : std::uint8_t {
+    FREQUENCY_115_HZ [[maybe_unused]] = 0b00,
+    FREQUENCY_230_HZ [[maybe_unused]] = 0b01,
+    FREQUENCY_460_HZ [[maybe_unused]] = 0b10,
+    FREQUENCY_920_HZ [[maybe_unused]] = 0b11,
+  };
 
-        enum SlowFilter : std::uint8_t {
-            SLOW_FILTER_X16 [[maybe_unused]] = 0b00,
-            SLOW_FILTER_X8 [[maybe_unused]] = 0b01,
-            SLOW_FILTER_X4 [[maybe_unused]] = 0b10,
-            SLOW_FILTER_X2 [[maybe_unused]] = 0b11,
-        };
+  enum SlowFilter : std::uint8_t {
+    SLOW_FILTER_X16 [[maybe_unused]] = 0b00,
+    SLOW_FILTER_X8 [[maybe_unused]] = 0b01,
+    SLOW_FILTER_X4 [[maybe_unused]] = 0b10,
+    SLOW_FILTER_X2 [[maybe_unused]] = 0b11,
+  };
 
-        enum FastFilterThreshold : std::uint16_t {
-            SLOW_FILTER_ONLY [[maybe_unused]] = 0b000,
-            FAST_FILTER_THRESHOLD_6_LSB [[maybe_unused]] = 0b001,
-            FAST_FILTER_THRESHOLD_7_LSB [[maybe_unused]] = 0b010,
-            FAST_FILTER_THRESHOLD_9_LSB [[maybe_unused]] = 0b011,
-            FAST_FILTER_THRESHOLD_18_LSB [[maybe_unused]] = 0b100,
-            FAST_FILTER_THRESHOLD_21_LSB [[maybe_unused]] = 0b101,
-            FAST_FILTER_THRESHOLD_24_LSB [[maybe_unused]] = 0b110,
-            FAST_FILTER_THRESHOLD_10_LSB [[maybe_unused]] = 0b111,
-        };
+  enum FastFilterThreshold : std::uint16_t {
+    SLOW_FILTER_ONLY [[maybe_unused]] = 0b000,
+    FAST_FILTER_THRESHOLD_6_LSB [[maybe_unused]] = 0b001,
+    FAST_FILTER_THRESHOLD_7_LSB [[maybe_unused]] = 0b010,
+    FAST_FILTER_THRESHOLD_9_LSB [[maybe_unused]] = 0b011,
+    FAST_FILTER_THRESHOLD_18_LSB [[maybe_unused]] = 0b100,
+    FAST_FILTER_THRESHOLD_21_LSB [[maybe_unused]] = 0b101,
+    FAST_FILTER_THRESHOLD_24_LSB [[maybe_unused]] = 0b110,
+    FAST_FILTER_THRESHOLD_10_LSB [[maybe_unused]] = 0b111,
+  };
 
-        struct __attribute__((__packed__)) Configuration {
-            PowerMode powerMode: 2;
-            Hysteresis hysteresis: 2;
-            OutputStage outputStage: 2;
-            PWMFrequency pwmFrequency: 2;
-            SlowFilter slowFilter: 2;
-            FastFilterThreshold fastFilterThreshold: 3;
-            bool watchdog: 1;
-            [[maybe_unused]] std::uint8_t reserved: 2;
-        };
+  struct __attribute__((__packed__)) Configuration {
+    PowerMode powerMode : 2;
+    Hysteresis hysteresis : 2;
+    OutputStage outputStage : 2;
+    PWMFrequency pwmFrequency : 2;
+    SlowFilter slowFilter : 2;
+    FastFilterThreshold fastFilterThreshold : 3;
+    bool watchdog : 1;
+    [[maybe_unused]] std::uint8_t reserved : 2;
+  };
 
-        struct __attribute__((__packed__)) Status {
-            [[maybe_unused]] std::uint8_t reserved_1: 3;
-            bool magnetHigh: 1;
-            bool magnetLow: 1;
-            bool magnetDetected: 1;
-            [[maybe_unused]] std::uint8_t reserved_2: 2;
-        };
+  struct __attribute__((__packed__)) Status {
+    [[maybe_unused]] std::uint8_t reserved_1 : 3;
+    bool magnetHigh : 1;
+    bool magnetLow : 1;
+    bool magnetDetected : 1;
+    [[maybe_unused]] std::uint8_t reserved_2 : 2;
+  };
 
-    public:
-        AS5600();
+public:
+  AS5600();
 
-        ~AS5600() override = default;
+  ~AS5600() override = default;
 
-    public:
-        [[maybe_unused]] void setPowerMode(PowerMode powerMode);
+public:
+  [[maybe_unused]] void setPowerMode(PowerMode powerMode);
 
-        [[maybe_unused]] void setHysteresis(Hysteresis hysteresis);
+  [[maybe_unused]] void setHysteresis(Hysteresis hysteresis);
 
-        [[maybe_unused]] void setOutputStage(OutputStage outputStage);
+  [[maybe_unused]] void setOutputStage(OutputStage outputStage);
 
-        [[maybe_unused]] void setPWMFrequency(PWMFrequency pwmFrequency);
+  [[maybe_unused]] void setPWMFrequency(PWMFrequency pwmFrequency);
 
-        [[maybe_unused]] void setSlowFilter(SlowFilter slowFilter);
+  [[maybe_unused]] void setSlowFilter(SlowFilter slowFilter);
 
-        [[maybe_unused]] void setFastFilterThreshold(FastFilterThreshold fastFilterThreshold);
+  [[maybe_unused]] void setFastFilterThreshold(FastFilterThreshold fastFilterThreshold);
 
-        [[maybe_unused]] void setWatchdog(bool enable);
+  [[maybe_unused]] void setWatchdog(bool enable);
 
+public:
+  Angle getMechanicalAngle() override;
 
-    public:
-        Angle getMechanicalAngle() override;
+  Angle getAngle() override;
 
-        Angle getAngle() override;
+  Angle getVelocity() override;
 
-        Angle getVelocity() override;
+  Rotations getFullRotations() override;
 
-        Rotations getFullRotations() override;
+  Rotations getRotations() override;
 
-        Rotations getRotations() override;
+  PreciseAngle getPreciseAngle() override;
 
-        PreciseAngle getPreciseAngle() override;
+public:
+  void update() override;
 
-    public:
-        void update() override;
+public:
+  int needsSearch() override;
 
-    public:
-        int needsSearch() override;
+protected:
+  Angle getSensorAngle() override;
 
-    protected:
-        Angle getSensorAngle() override;
+  void init() override;
 
-        void init() override;
+public:
+  [[nodiscard]] AS5600::Status getStatus();
 
-    public:
-        [[nodiscard]] AS5600::Status getStatus();
+  [[nodiscard]] AS5600::Configuration getConfiguration();
 
-        [[nodiscard]] AS5600::Configuration getConfiguration();
+private:
+  AS5600::Device m_device = nullptr;
+  AS5600::BusMaster m_busMaster = nullptr;
 
-    private:
-        AS5600::Device m_device = nullptr;
-        AS5600::BusMaster m_busMaster = nullptr;
+private:
+  Status m_status = {};
+  Configuration m_configuration = {};
+};
 
-    private:
-        Status m_status = {};
-        Configuration m_configuration = {};
-    };
-
-}
+} // namespace foc
--- src/AS5600.cpp
+++ src/AS5600.cpp
@@ -20,184 +20,168 @@
 
 auto const TAG = "AS5600";
 
-[[maybe_unused]] auto const DEVICE_ADDRESS = 0x36;    ///< Адрес устройства
-[[maybe_unused]] auto const REGISTER_ZMCO = 0x00;     ///< Количество записей
-[[maybe_unused]] auto const REGISTER_ZPOS = 0x01;     ///< Минимальное положение
-[[maybe_unused]] auto const REGISTER_MPOS = 0x03;     ///< Максимальное положение
-[[maybe_unused]] auto const REGISTER_MANG = 0x05;     ///< Диапазон значений
-[[maybe_unused]] auto const REGISTER_CONF = 0x07;     ///<  Конфигурация
-[[maybe_unused]] auto const REGISTER_STATUS = 0x0B;   ///< Статус
-[[maybe_unused]] auto const REGISTER_RAW_ANGLE = 0x0C;///< Угол (сырое значение)
-[[maybe_unused]] auto const REGISTER_ANGLE = 0x0E;    ///< Угол (значение после фильтрации)
-[[maybe_unused]] auto const REGISTER_AGC = 0x1A;      ///< Automatic Gain Control
-[[maybe_unused]] auto const REGISTER_MAGNITUDE = 0x1B;///< Магнитуда
-[[maybe_unused]] auto const REGISTER_BURN = 0xFF;     ///< Запись в ПЗУ
+[[maybe_unused]] auto const DEVICE_ADDRESS = 0x36;     ///< Адрес устройства
+[[maybe_unused]] auto const REGISTER_ZMCO = 0x00;      ///< Количество записей
+[[maybe_unused]] auto const REGISTER_ZPOS = 0x01;      ///< Минимальное положение
+[[maybe_unused]] auto const REGISTER_MPOS = 0x03;      ///< Максимальное положение
+[[maybe_unused]] auto const REGISTER_MANG = 0x05;      ///< Диапазон значений
+[[maybe_unused]] auto const REGISTER_CONF = 0x07;      ///<  Конфигурация
+[[maybe_unused]] auto const REGISTER_STATUS = 0x0B;    ///< Статус
+[[maybe_unused]] auto const REGISTER_RAW_ANGLE = 0x0C; ///< Угол (сырое значение)
+[[maybe_unused]] auto const REGISTER_ANGLE = 0x0E;     ///< Угол (значение после фильтрации)
+[[maybe_unused]] auto const REGISTER_AGC = 0x1A;       ///< Automatic Gain Control
+[[maybe_unused]] auto const REGISTER_MAGNITUDE = 0x1B; ///< Магнитуда
+[[maybe_unused]] auto const REGISTER_BURN = 0xFF;      ///< Запись в ПЗУ
 
-template<typename T>
-T convertBytesToData(std::vector<std::uint8_t> bytes) {
-    T struct_{};
+namespace {
 
-    std::reverse(bytes.begin(), bytes.end());
-    std::memcpy(&struct_, bytes.data(), bytes.size());
+template <typename T> T convertBytesToData(std::vector<std::uint8_t> bytes) {
+  T struct_{};
 
-    return struct_;
+  std::reverse(bytes.begin(), bytes.end());
+  std::memcpy(&struct_, bytes.data(), bytes.size());
+
+  return struct_;
 }
 
-template<typename T>
-std::vector<uint8_t> convertDataToBytes(T struct_) {
-    std::vector<uint8_t> bytes(sizeof(T));
+template <typename T> std::vector<uint8_t> convertDataToBytes(T struct_) {
+  std::vector<uint8_t> bytes(sizeof(T));
 
-    std::memcpy(bytes.data(), &struct_, sizeof(T));
-    std::reverse(bytes.begin(), bytes.end());
+  std::memcpy(bytes.data(), &struct_, sizeof(T));
+  std::reverse(bytes.begin(), bytes.end());
 
-    return bytes;
+  return bytes;
 }
+
+} // namespace
 
 namespace foc {
 
-    AS5600::AS5600() {
-        m_busMaster = std::make_unique<i2c::Master>(21, 22, 0);
-        m_device = m_busMaster->createDevice(DEVICE_ADDRESS);
+AS5600::AS5600() {
+  m_busMaster = std::make_unique<i2c::Master>(21, 22, 0);
+  m_device = m_busMaster->createDevice(DEVICE_ADDRESS);
 
-        m_status = getStatus();
-        ESP_LOGI(TAG, "Magnet low: %d", m_status.magnetLow);
-        ESP_LOGI(TAG, "Magnet high: %d", m_status.magnetHigh);
-        ESP_LOGI(TAG, "Magnet detected: %d", m_status.magnetDetected);
+  m_status = getStatus();
+  ESP_LOGI(TAG, "Magnet low: %d", m_status.magnetLow);
+  ESP_LOGI(TAG, "Magnet high: %d", m_status.magnetHigh);
+  ESP_LOGI(TAG, "Magnet detected: %d", m_status.magnetDetected);
 
-        m_configuration = getConfiguration();
-        ESP_LOGI(TAG, "Power mode: %d", m_configuration.powerMode);
-        ESP_LOGI(TAG, "Hysteresis: %d", m_configuration.hysteresis);
-        ESP_LOGI(TAG, "Output stage: %d", m_configuration.outputStage);
-        ESP_LOGI(TAG, "PWM Frequency: %d", m_configuration.pwmFrequency);
-        ESP_LOGI(TAG, "Slow filter: %d", m_configuration.slowFilter);
-        ESP_LOGI(TAG, "Fast filter threshold: %d", m_configuration.fastFilterThreshold);
-        ESP_LOGI(TAG, "Watchdog: %d", m_configuration.watchdog);
-    }
-
-    [[maybe_unused]] void AS5600::setPowerMode(AS5600::PowerMode powerMode) {
-        m_configuration.powerMode = powerMode;
-
-        auto const configurationData = convertDataToBytes(m_configuration);
-
-        m_device->write(REGISTER_CONF, configurationData);
-    }
-
-    [[maybe_unused]] void AS5600::setHysteresis(AS5600::Hysteresis hysteresis) {
-        m_configuration.hysteresis = hysteresis;
-
-        auto const configurationData = convertDataToBytes(m_configuration);
-
-        m_device->write(REGISTER_CONF, configurationData);
-    }
-
-    [[maybe_unused]] void AS5600::setOutputStage(AS5600::OutputStage outputStage) {
-        m_configuration.outputStage = outputStage;
-
-        auto const configurationData = convertDataToBytes(m_configuration);
-
-        m_device->write(REGISTER_CONF, configurationData);
-    }
-
-    [[maybe_unused]] void AS5600::setPWMFrequency(AS5600::PWMFrequency pwmFrequency) {
-        m_configuration.pwmFrequency = pwmFrequency;
-
-        auto const configurationData = convertDataToBytes(m_configuration);
-
-        m_device->write(REGISTER_CONF, configurationData);
-    }
-
-    [[maybe_unused]] void AS5600::setSlowFilter(AS5600::SlowFilter slowFilter) {
-        m_configuration.slowFilter = slowFilter;
-
-        auto const configurationData = convertDataToBytes(m_configuration);
-
-        m_device->write(REGISTER_CONF, configurationData);
-    }
-
-    [[maybe_unused]] void AS5600::setFastFilterThreshold(AS5600::FastFilterThreshold fastFilterThreshold) {
-        m_configuration.fastFilterThreshold = fastFilterThreshold;
-
-        auto const configurationData = convertDataToBytes(m_configuration);
-
-        m_device->write(REGISTER_CONF, configurationData);
-    }
-
-    [[maybe_unused]] void AS5600::setWatchdog(bool enable) {
-        m_configuration.watchdog = enable;
-
-        auto const configurationData = convertDataToBytes(m_configuration);
-
-        m_device->write(REGISTER_CONF, configurationData);
-    }
-
-    AS5600::Status AS5600::getStatus() {
-        if (not m_device) {
-            return m_status;
-        }
-
-        auto const statusData = m_device->read(REGISTER_STATUS);
-
-        m_status = convertBytesToData<AS5600::Status>(statusData);
-
-        return m_status;
-    }
-
-    AS5600::Configuration AS5600::getConfiguration() {
-        if (not m_device) {
-            return m_configuration;
-        }
-
-        auto const configurationData = m_device->read(REGISTER_CONF, 2);
-
-        m_configuration = convertBytesToData<AS5600::Configuration>(configurationData);
-
-        return m_configuration;
-    }
-
-    float AS5600::getMechanicalAngle() {
-        return EncoderBase::getMechanicalAngle();
-    }
-
-    float AS5600::getAngle() {
-        return EncoderBase::getAngle();
-    }
-
-    float AS5600::getVelocity() {
-        return EncoderBase::getVelocity();
-    }
-
-    AS5600::Rotations AS5600::getFullRotations() {
-        return EncoderBase::getFullRotations();
-    }
-
-    AS5600::Rotations AS5600::getRotations() {
-        return AS5600::Rotations();
-    }
-
-    AS5600::PreciseAngle AS5600::getPreciseAngle() {
-        return EncoderBase::getPreciseAngle();
-    }
-
-    void AS5600::update() {
-        EncoderBase::update();
-    }
-
-    int AS5600::needsSearch() {
-        return EncoderBase::needsSearch();
-    }
-
-    float AS5600::getSensorAngle() {
-        if (not m_device) {
-            return {};
-        }
-
-        auto const angleData = m_device->read(REGISTER_ANGLE, 2);
-
-        return convertBytesToData<AS5600::Angle>(angleData);
-    }
-
-    void AS5600::init() {
-        EncoderBase::init();
-    }
-
+  m_configuration = getConfiguration();
+  ESP_LOGI(TAG, "Power mode: %d", m_configuration.powerMode);
+  ESP_LOGI(TAG, "Hysteresis: %d", m_configuration.hysteresis);
+  ESP_LOGI(TAG, "Output stage: %d", m_configuration.outputStage);
+  ESP_LOGI(TAG, "PWM Frequency: %d", m_configuration.pwmFrequency);
+  ESP_LOGI(TAG, "Slow filter: %d", m_configuration.slowFilter);
+  ESP_LOGI(TAG, "Fast filter threshold: %d", m_configuration.fastFilterThreshold);
+  ESP_LOGI(TAG, "Watchdog: %d", m_configuration.watchdog);
 }
+
+[[maybe_unused]] void AS5600::setPowerMode(AS5600::PowerMode powerMode) {
+  m_configuration.powerMode = powerMode;
+
+  auto const configurationData = convertDataToBytes(m_configuration);
+
+  m_device->write(REGISTER_CONF, configurationData);
+}
+
+[[maybe_unused]] void AS5600::setHysteresis(AS5600::Hysteresis hysteresis) {
+  m_configuration.hysteresis = hysteresis;
+
+  auto const configurationData = convertDataToBytes(m_configuration);
+
+  m_device->write(REGISTER_CONF, configurationData);
+}
+
+[[maybe_unused]] void AS5600::setOutputStage(AS5600::OutputStage outputStage) {
+  m_configuration.outputStage = outputStage;
+
+  auto const configurationData = convertDataToBytes(m_configuration);
+
+  m_device->write(REGISTER_CONF, configurationData);
+}
+
+[[maybe_unused]] void AS5600::setPWMFrequency(AS5600::PWMFrequency pwmFrequency) {
+  m_configuration.pwmFrequency = pwmFrequency;
+
+  auto const configurationData = convertDataToBytes(m_configuration);
+
+  m_device->write(REGISTER_CONF, configurationData);
+}
+
+[[maybe_unused]] void AS5600::setSlowFilter(AS5600::SlowFilter slowFilter) {
+  m_configuration.slowFilter = slowFilter;
+
+  auto const configurationData = convertDataToBytes(m_configuration);
+
+  m_device->write(REGISTER_CONF, configurationData);
+}
+
+[[maybe_unused]] void AS5600::setFastFilterThreshold(AS5600::FastFilterThreshold fastFilterThreshold) {
+  m_configuration.fastFilterThreshold = fastFilterThreshold;
+
+  auto const configurationData = convertDataToBytes(m_configuration);
+
+  m_device->write(REGISTER_CONF, configurationData);
+}
+
+[[maybe_unused]] void AS5600::setWatchdog(bool enable) {
+  m_configuration.watchdog = enable;
+
+  auto const configurationData = convertDataToBytes(m_configuration);
+
+  m_device->write(REGISTER_CONF, configurationData);
+}
+
+AS5600::Status AS5600::getStatus() {
+  if (not m_device) {
+    return m_status;
+  }
+
+  auto const statusData = m_device->read(REGISTER_STATUS);
+
+  m_status = convertBytesToData<AS5600::Status>(statusData);
+
+  return m_status;
+}
+
+AS5600::Configuration AS5600::getConfiguration() {
+  if (not m_device) {
+    return m_configuration;
+  }
+
+  auto const configurationData = m_device->read(REGISTER_CONF, 2);
+
+  m_configuration = convertBytesToData<AS5600::Configuration>(configurationData);
+
+  return m_configuration;
+}
+
+float AS5600::getMechanicalAngle() { return EncoderBase::getMechanicalAngle(); }
+
+float AS5600::getAngle() { return EncoderBase::getAngle(); }
+
+float AS5600::getVelocity() { return EncoderBase::getVelocity(); }
+
+AS5600::Rotations AS5600::getFullRotations() { return EncoderBase::getFullRotations(); }
+
+AS5600::Rotations AS5600::getRotations() { return AS5600::Rotations(); }
+
+AS5600::PreciseAngle AS5600::getPreciseAngle() { return EncoderBase::getPreciseAngle(); }
+
+void AS5600::update() { EncoderBase::update(); }
+
+int AS5600::needsSearch() { return EncoderBase::needsSearch(); }
+
+float AS5600::getSensorAngle() {
+  if (not m_device) {
+    return {};
+  }
+
+  auto const angleData = m_device->read(REGISTER_ANGLE, 2);
+
+  return convertBytesToData<AS5600::Angle>(angleData);
+}
+
+void AS5600::init() { EncoderBase::init(); }
+
+} // namespace foc
